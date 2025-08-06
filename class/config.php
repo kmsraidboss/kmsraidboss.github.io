15a9c05ca0a955c92cc57a548de931c29566f525
Michael <admin@raidboss.org>
2025-08-06 10:08:56 -0400

    Initial upload

---
+++ class/config.php
@@ -0,0 +1,16 @@
+<?php
+define('SKIN_LANGUAGE', 'skins_en');
+
+define('DB_HOST', '66.85.14.233');
+define('DB_PORT', '3306');
+define('DB_NAME', 'srh_ws');
+define('DB_USER', 'kmsraidboss');
+define('DB_PASS', 'ALiCinSbASPHEarB');
+
+define('WEB_STYLE_DARK', true);
+
+define('STEAM_API_KEY', '2686A4B4171CD1887195FA34E5A61B5F');
+define('STEAM_DOMAIN_NAME', '');
+define('STEAM_LOGOUT_PAGE', '');
+define('STEAM_LOGIN_PAGE', '');
+
